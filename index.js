9d4906a7b9b8d23ddc4ad721cc9f3c510280cb76
Tidish <tidish@inbox.lt>
2019-05-13 19:49:44 +0300

    Added path

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
