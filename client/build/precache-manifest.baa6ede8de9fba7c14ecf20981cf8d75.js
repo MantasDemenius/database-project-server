f247e6013b6de6528cf798d3b0f19e4534f69a21
Tidish <tidish@inbox.lt>
2019-05-13 20:22:09 +0300

    Added client to heroku server

---
+++ client/build/precache-manifest.baa6ede8de9fba7c14ecf20981cf8d75.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/database-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "f5b1ecdcb883584c3b4c",
+    "url": "/database-project/static/css/main.ae6ce581.chunk.css"
+  },
+  {
+    "revision": "69d0936fdb579753eaf5",
+    "url": "/database-project/static/js/runtime~main.675ef7be.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/database-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "612e87772d69cbcbdb87",
+    "url": "/database-project/static/js/2.9145d352.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/database-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/database-project/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/database-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/database-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/database-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/database-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "f5b1ecdcb883584c3b4c",
+    "url": "/database-project/static/js/main.eb1d9639.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/database-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/database-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/database-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/database-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/database-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/database-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/database-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/database-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "612e87772d69cbcbdb87",
+    "url": "/database-project/static/css/2.5366cb87.chunk.css"
+  },
+  {
+    "revision": "5d6a9bb89c71807e94ec06e5772dc001",
+    "url": "/database-project/index.html"
+  }
+];
