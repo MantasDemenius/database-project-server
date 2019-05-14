478c0ae9abb9c3f95dbe8ed2b7ce6c9a3a02d153
Tidish <tidish@inbox.lt>
2019-05-14 16:09:55 +0300

    changes

--- routes/CompaniesRoute.js
+++ routes/CompaniesRoute.js
@@ -7,8 +7,13 @@ module.exports = (app, conn) => {
 
   app.get('/database-project/Companies', (req, res) => {
     conn.query("SELECT * FROM imone", (err, data) => {
-      if (err) throw err;
-      res.json({results: data});
+      if (err) {
+        res.status(500).json({ errors: {globalErr: err } });
+      }
+      else{
+        res.status(200).json({results: data});
+      }
+
     });
   });
 
