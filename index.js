b6c544c5ca8daccbcb9e58f09db6e6351b633624
Tidish <tidish@inbox.lt>
2019-05-23 10:35:24 +0300

    Disconnect handling

--- index.js
+++ index.js
@@ -19,18 +19,41 @@ app.use(express.json());
 
 // my CLEARDB_DATABASE_URL: mysql://b9e4b9609169bd:ef98abad05b8d68@us-cdbr-iron-east-02.cleardb.net/heroku_4a047a9c92e88c3?reconnect=true
 //Heroku CLEARDB
-const conn = mysql.createConnection({
+var connectionString = {
   host     : 'us-cdbr-iron-east-02.cleardb.net',
   user     : 'b9e4b9609169bd',
   password : 'ef98abad05b8d68',
   database : 'heroku_4a047a9c92e88c3',
   dateStrings: 'date'
-});
+};
+
+const conn = mysql.createConnection(connectionString);
 
 conn.connect(function(err){
   (err)? console.log(err) : console.log("all good my man with connection");
 });
 
+function handleDisconnect() {
+  connection = mysql.createConnection(connectionString); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
 // Serve any static files built by React
 // app.use(express.static(path.join(__dirname, "client/build")));
 //
