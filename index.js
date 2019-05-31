aa3851800a3f90bb2fce6e23d678c38c06ce1f23
Tidish <tidish@inbox.lt>
2019-05-31 12:59:26 +0300

    Path for all

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ require('./routes/CommentsRoute')(app, conn);
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     // res.sendFile(path.join(__dirname+'/client/build/index.html'));
-    res.send("Fail")l
+    res.send("Fail");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
